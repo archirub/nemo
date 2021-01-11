300a5d68edcdc096b70dd0a1d8226d950e56d211
kjturan <kaanjenkinsturan@gmail.com>
2021-01-11 21:09:00 +0000

    Minor routing change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,10 +40,10 @@ const routes: Routes = [
     path: "**",
     redirectTo: "tabs/home",
   },
-  {
-    path: 'profile-card',
-    loadChildren: () => import('./pages/profile-card/profile-card.module').then( m => m.ProfileCardPageModule)
-  },
+  // {
+  //   path: 'profile-card',
+  //   loadChildren: () => import('./pages/profile-card/profile-card.module').then( m => m.ProfileCardPageModule)
+  // },
 ];
 
 @NgModule({
