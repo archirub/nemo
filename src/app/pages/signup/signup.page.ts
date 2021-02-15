8892a1b588fa5b42ede52b14caaf3278bb9fe048
ewanmiles <mrewanmiles@hotmail.com>
2021-02-15 15:58:03 +0000

    - Full draft of the signup pages - Very slight issues that need to be remedied, including terms checkboxes

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -41,43 +41,116 @@ export class SignupPage implements OnInit {
     this.pagerUpdate();
   }
 
-  unlockAndSwipe(direction) {
+  async unlockAndSwipe(direction) {
     this.slides.lockSwipes(false);
 
     if (direction=="next") {
       this.slides.slideNext()
     } else if (direction=="prev") {
       this.slides.slidePrev()
+    } else if (typeof direction === "number") {
+      this.slides.slideTo(direction)
     } else {
       console.log("Correct direction not provided.");
     };
 
     this.slides.lockSwipes(true);
     this.pagerUpdate();
+
+    var current = await this.slides.getActiveIndex();
+    if (current==6) {
+      this.waveFade();
+    };
   }
 
+  /* Updates the pager to current slide, run on every unlockAndSwipe() */
   async pagerUpdate() {
+    var pager:any = document.getElementsByClassName("pager")[0];
     var logos = [document.getElementsByName("mail-outline")[0],
-                document.getElementsByName("shield-outline")[0],
-                document.getElementsByName("person-outline")[0],
                 document.getElementsByName("ellipsis-horizontal")[0],
+                document.getElementsByName("person")[0],
                 document.getElementsByName("gift-outline")[0],
                 document.getElementsByName("camera-outline")[0],
-                document.getElementsByName("happy-outline")[0]];
+                document.getElementsByName("happy-outline")[0],
+                document.getElementsByName("school-outline")[0],
+                document.getElementsByName("school-outline")[1], //Placeholder icon for waves slide
+                document.getElementsByName("location-outline")[0],
+                document.getElementsByName("list-outline")[0],
+                document.getElementsByName("color-palette-outline")[0],
+                document.getElementsByName("accessibility")[0],
+                document.getElementsByName("person-add")[0]];
     var pages:any = document.getElementsByClassName("pager-dot");
     var current = await this.slides.getActiveIndex();
-    var pagesLeft = 6 - current;
 
-    /* Initially hide all logos */
-    logos.forEach(element => element.style.display = "none");
-    logos[current].style.display = "block";
+    if (current < 6) {
+      var pagesLeft = 5 - current;
+    } else {
+      var pagesLeft = 12 - current;
+      for (let i = 0; i < pages.length; i++) {
+        pages[i].style.display = "block";
+      };
+    };
+    
+    /* Remove pager for the waves slide */
+    if (current === 6) {
+      pager.style.display = "none";
+    } else {
+      pager.style.display = "flex";
 
-    for (let i = 0; i < pages.length; i++) {
-      if (i+1 > pagesLeft) {
-        pages[i].style.display = "none";
+      // Initially hide all logos
+      logos.forEach(element => element.style.display = "none");
+      logos[current].style.display = "block";
+
+      for (let i = 0; i < pages.length; i++) {
+        if (i+1 > pagesLeft) {
+          pages[i].style.display = "none";
+        };
       };
     };
   }
+
+  /* Fade in/out basic pure JS animations */
+  fadeIn(HTMLElement) {
+    for (let i = 0; i < 11; i ++) {
+      setTimeout(() => {
+        HTMLElement.style.opacity = i/10;
+      }, 50);
+    }
+  }
+
+  fadeOut(HTMLElement) {
+    var fade = setInterval(() => {
+      if (HTMLElement.style.opacity > 0) {
+        HTMLElement.style.opacity -= 0.1;
+      } else {
+        clearInterval(fade);
+      }
+    }, 50)
+  }
+
+  /* Fades between waves backgrounds and text on slide 6 */
+  waveFade() {
+    var slides:any = [document.getElementsByClassName("waves")[0],
+                  document.getElementsByClassName("waves")[1]]; //Wave backgrounds
+
+    var text:any = [document.getElementsByClassName("blow-up")[0],
+                document.getElementsByClassName("blow-up")[1]]; //Text h1s
+
+    var inFade = setInterval(() => {
+      if (slides[0].style.opacity != 0) { //Fade out first slide
+        this.fadeOut(slides[0]);
+        this.fadeIn(slides[1]);
+        this.fadeOut(text[0]);
+        this.fadeIn(text[1]);
+      } else if (slides[1].style.opacity != 0) { //Fade out second slide
+        this.fadeOut(slides[1]);
+        this.fadeIn(slides[0]);
+        this.fadeOut(text[1]);
+        this.fadeIn(text[0]);
+      };
+    }, 5000)
+  }
+
 
   // Since we are restricted by the two options we have
   // for image picking (i.e. the Capacitor Camera plugin is great
