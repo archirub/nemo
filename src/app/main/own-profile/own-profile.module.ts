cec64b04c5495b26c3481dcd0621de1e958c0768
kjturan <kaanjenkinsturan@gmail.com>
2021-02-24 19:17:48 +0000

    Minor bug fixes

--- src/app/main/own-profile/own-profile.module.ts
+++ src/app/main/own-profile/own-profile.module.ts
@@ -7,15 +7,17 @@ import { IonicModule } from "@ionic/angular";
 import { OwnProfilePageRoutingModule } from "./own-profile-routing.module";
 
 import { OwnProfilePage } from "./own-profile.page";
-import { ProfileCardComponent, AddPhotoComponent, AppToggleComponent } from "@components/index";
+import { AddPhotoComponent, AppToggleComponent } from "@components/index";
+import { ProfileCardModule } from "@components/profile-card/profile-card.component.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ProfileCardModule,
     OwnProfilePageRoutingModule,
   ],
-  declarations: [OwnProfilePage, ProfileCardComponent, AddPhotoComponent, AppToggleComponent],
+  declarations: [OwnProfilePage, AddPhotoComponent, AppToggleComponent],
 })
 export class OwnProfilePageModule {}
