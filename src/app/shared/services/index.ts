d481758da8858a45efacea39b313740b5da440ad
Ewan <ewan.a.miles@gmail.com>
2021-07-14 17:12:34 +0100

    - UI and auth service sorted for incorrect logins

--- src/app/shared/services/index.ts
+++ src/app/shared/services/index.ts
@@ -1,5 +1,6 @@
 export * from "./loading/loading.service";
 export * from "./format/format.service";
+export * from "./firebase-auth/firebase-auth.service";
 
 // these two (init and signup) are commented out and must imported by writing out their folder path instead
 // to avoid circular dependencies that having them exported from "index" creates
--- src/app/welcome/login/login.page.ts
+++ src/app/welcome/login/login.page.ts
@@ -4,6 +4,8 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 import { AngularFireAuth } from "@angular/fire/auth";
 
+import { FirebaseAuthService } from "@services/index";
+
 import { FishSwimAnimation } from "@animations/index";
 
 @Component({
@@ -15,6 +17,8 @@ export class LoginPage {
   @ViewChild("fish", { read: ElementRef }) fish: ElementRef;
   fishSwimAnimation;
 
+  errorMsg: string;
+
   loginForm = new FormGroup({
     email: new FormControl("", [
       Validators.email,
@@ -24,7 +28,11 @@ export class LoginPage {
     password: new FormControl("", [Validators.required]),
   });
 
-  constructor(private alertCtrl: AlertController, private afAuth: AngularFireAuth) {}
+  constructor (
+    private alertCtrl: AlertController, 
+    private afAuth: AngularFireAuth,
+    private authService: FirebaseAuthService
+    ) {}
 
   ionViewDidEnter() {
     //Initialise animations
@@ -42,7 +50,20 @@ export class LoginPage {
     const email: string = this.loginForm.get("email").value;
     const password: string = this.loginForm.get("password").value;
 
-    this.afAuth.signInWithEmailAndPassword(email, password);
+    this.afAuth.signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        if (error.code === 'auth/wrong-password') {
+          this.authService.wrongPasswordPopup(null);
+        } else if (error.code === 'auth/user-not-found') {
+          this.authService.unknownUserPopup();
+        } else if (error.code === 'auth/user-disabled') {
+          this.authService.userDisabledPopup();
+        } else if (error.code === 'auth/too-many-requests') {
+          this.authService.wrongPasswordMaxAttemptsPopup(null);
+        } else {
+            this.authService.unknownErrorPopup();
+        };
+      });
   }
 
   enterSubmit(event) {
